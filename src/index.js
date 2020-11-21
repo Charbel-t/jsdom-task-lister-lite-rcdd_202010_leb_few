f4e477ad64308ee0c3b76192dcb38c77c50b0f95
Charbel t <charbel2000.52@gmail.com>
2020-11-21 09:18:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   //grab all the necessary DOM elements
   //form and relevant input fields
   const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
+  //const newTaskDescription = document.getElementById("new-task-description");
+  //const newTaskPriority = document.getElementById("new-task-priority");
   //ul where new tasks will live on the DOM
-  const newTaskUl = document.getElementById("tasks");
-
+  //const newTaskUl = document.getElementById("tasks");
+  
   //attach event listeners
   newTaskForm.addEventListener("submit", createNewTask);
 });
